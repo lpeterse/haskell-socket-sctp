fe572d8b5df33dfdb27336421d76bec689feea41
Lars Petersen <lars@nyantec.com>
2015-06-22 22:29:46 +0200

    Finalised SendReceiveMessage test-suite.

--- tests/SendReceiveMessage.hs
+++ tests/SendReceiveMessage.hs
@@ -1,12 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Bits
 import Data.Monoid
 import Control.Monad
 import Control.Exception
 import Control.Concurrent
-import Control.Concurrent.Async
 
 import System.Socket
 import System.Socket.Family.Inet as Inet
@@ -14,19 +12,30 @@ import System.Socket.Protocol.SCTP as SCTP
 
 main :: IO ()
 main = do
-  server <- socket                        `onException` p 0 :: IO (Socket Inet SequentialPacket SCTP)
-  client <- socket                        `onException` p 1 :: IO (Socket Inet SequentialPacket SCTP)
-  bind server addr                        `onException` p 3
-  listen server 5                         `onException` p 4
-  connect client addr                     `onException` p 5
+  server <- socket                  `onException` p 0 :: IO (Socket Inet SequentialPacket SCTP)
+  client <- socket                  `onException` p 1 :: IO (Socket Inet SequentialPacket SCTP)
+  bind server addr                  `onException` p 3
+  listen server 5                   `onException` p 4
+  connect client addr               `onException` p 5
+  setSocketOption
+    server 
+    (mempty { dataIOEvent = True }) `onException` p 6
+  SCTP.sendMessage
+    client
+    "hallo"
+    addr
+    ( 2342   :: PayloadProtocolIdentifier )
+    ( mempty :: MessageFlags )
+    ( 2      :: StreamNumber )
+    ( 3      :: TimeToLive )
+    ( 4      :: Context )           `onException` p 7
+  (msg, adr, sinfo, flags) <- SCTP.receiveMessage server 4096 mempty
+                                    `onException` p 8
 
-  setSocketOption server (mempty { dataIOEvent = True })
-
-  SCTP.sendMessage client "hallo" addr 1 mempty 2 3 4 `onException` p 6
-  x@(msg, adr, sinfo, flags) <- SCTP.receiveMessage server 4096 mempty
-
-  print x
-
+  when (sinfoPayloadProtocolIdentifier sinfo /= 2342) (e 9)
+  when (sinfoStreamNumber sinfo /= 2) (e 10)
+  when (msg /= "hallo") (e 11)
+  when (flags /= msgEndOfRecord) (e 12)
 
 addr :: SocketAddressInet
 addr  = SocketAddressInet Inet.loopback 7777
